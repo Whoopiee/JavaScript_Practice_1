346e125e828d9d9005660ac31d56624298f03d75
Valery <val.marchenko4@gmail.com>
2022-07-04 19:08:11 +0300

    Added validation of input data

--- talkWithUser1.js
+++ talkWithUser1.js
@@ -1,9 +1,21 @@
 "use scrict";
 
 const numOfQuestions = 2; /*constant that have number of questions about films*/
+let numberOfFilms = 0;
 
-const numberOfFilms = prompt('Скільки фільмів ви вже подивились?','');
+while(true)
+{
+    numberOfFilms = prompt(`Скільки фільмів ви вже подивились?`,``);
 
+    if( numberOfFilms == null || numberOfFilms.length == 0 || numberOfFilms.length >50)
+    {
+        console.log(`Помилка!`);
+    }
+    else
+    {
+        break;
+    }
+}
 const personalMovieDB = {
     count: numberOfFilms,
     movie: {},
@@ -16,7 +28,18 @@ for(let numOfMovies=0;numOfMovies<numOfQuestions;numOfMovies++)
 {
     const firstQuestion = prompt(`Останній переглянутий фільм?`,``);
     const secondQuestion = prompt(`Скількі балів ви дасте цьому фільму?`,``);
+    
+    if(firstQuestion!=null && secondQuestion !=null && firstQuestion!='' && secondQuestion!='' && firstQuestion.length<50)
+    {
     personalMovieDB.movie[firstQuestion] = secondQuestion;
+    }else{
+        alert(`Помилка!`);
+    }
 }
+
+if(personalMovieDB.count<10){alert(`Доволі мало фільмів переглянуто`);}
+else if(personalMovieDB.count>=10 && personalMovieDB.count<=30 ){alert(`Ви типовий глядач!`);}
+else if (personalMovieDB.count>30){alert(`Ви кіноман!`);}
+else {alert(`Помилка!`);}
 
 console.log(personalMovieDB);
