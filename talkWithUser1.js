037ecf07dd5c9dab08418718220484d84a14f42e
Marchenko Valery <val.marchenko4@gmail.com>
2022-11-18 11:17:56 +0200

    Slightly reworked the code view

--- talkWithUser1.js
+++ talkWithUser1.js
@@ -2,7 +2,7 @@
 
 const numOfQuestions = 2; /*constant that have number of questions about films*/
 
-/*Function that checks input string for conditions*/
+//Function that checks input string for conditions
 function inputCheck(check){
     if(check==null || check=='' || check.length>50) 
     {
@@ -13,15 +13,16 @@ function inputCheck(check){
 }
 
 
-//NumOfFilmsInputCheck();
+//Class description 
 
 const personalMovieDB = {
     count: 0,
     movie: {},
     actors: {},
     genres: [],
-    private:false,
+    private: true,
 
+//Functions for polling the user and entering data into the class.
     NumOfFilmsInputCheck: function(){
         while(true)
         {
@@ -31,7 +32,7 @@ const personalMovieDB = {
     },
 
     addLastSeenFilm: function(){
-        for(let numOfMovies=0;numOfMovies<numOfQuestions;numOfMovies++)
+    for(let numOfMovies = 0; numOfMovies < numOfQuestions; numOfMovies++)
     {
         let firstQuestion,
             secondQuestion;
@@ -54,9 +55,9 @@ const personalMovieDB = {
     },
 
     checkPersonalLevel: function(){
-        if(this.count<10){alert(`Доволі мало фільмів переглянуто`);}
-        else if(this.count>=10 && this.count<=30 ){alert(`Ви типовий глядач!`);}
-        else if (this.count>30){alert(`Ви кіноман!`);}
+        if(this.count < 10){alert(`Доволі мало фільмів переглянуто`);}
+        else if(this.count >= 10 && this.count <= 30 ){alert(`Ви типовий глядач!`);}
+        else if (this.count > 30){alert(`Ви кіноман!`);}
         else {alert(`Помилка!`);}
     },
 
@@ -70,17 +71,17 @@ const personalMovieDB = {
     enterYourFavGenres: function(){
         let favGenre;
     
-        for(let i = 0 ; i<3; ++i)
+        for(let i = 0 ; i < 3; ++i)
         {
             while(true)
             {
-                favGenre = prompt(`Ваш улюблений жанр під номером ${i+1}`,``).trim();
+                favGenre = prompt(`Ваш улюблений жанр під номером ${i+1}`, ``).trim();
                 if(inputCheck(favGenre) && isNaN(favGenre))break;
             }
             this.genres.push(favGenre);
         }
 
-        this.genres.forEach((item,num)=>{
+        this.genres.forEach((item, num)=>{
             console.log(`Улюблений жанр #${num+1} - це ${item}`);
         });
     },
@@ -90,16 +91,41 @@ const personalMovieDB = {
     }
 };
 
-//personalMovieDB.NumOfFilmsInputCheck();
+personalMovieDB.NumOfFilmsInputCheck();
 
-//personalMovieDB.addLastSeenFilm();
+personalMovieDB.addLastSeenFilm();
 
-//personalMovieDB.checkPersonalLevel();
+personalMovieDB.checkPersonalLevel();
 
 personalMovieDB.toggleVisibleMyDB();
 
 personalMovieDB.showMyDB();
 
-
 personalMovieDB.enterYourFavGenres();
 
+
+
+//additional practice. Function that works with length of arrays
+
+function likes(names) {
+switch(names.length)
+{
+    case 0:{
+        return('no one likes this');
+        }
+    case 1:{
+        return(`${names} likes this`);
+        }
+    case 2:{
+        return(names.join(' and ') + ' like this');
+        }
+    case 3:{
+        return(`${names[0]}, ${names[1]} and ${names[2]} like this`);
+    }
+    default:{
+        return(`${names[0]}, ${names[1]} and ${names.length-2} others like this`);
+    }
+}
+  }
+
+  console.log(likes(['Max', 'John', 'Mark']));
